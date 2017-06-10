32d059b539976bf64bc1a22829d05880597069a5
Will Speak <lithiumflame@gmail.com>
2017-06-10 15:33:21 +0100

    Add Linux Build Support
    Update the *nix build script to compile on Linux. Switches to using
    the `dotnet` cli rather than nuget to create the packages.
    
    Fixes #1

--- build.sh
+++ build.sh
@@ -2,8 +2,30 @@
 
 set -e
 
-rid=osx
-platform=Darwin
+# Check for the programs we need to do the build
+
+if ! (which git && which cargo && which dotnet)>/dev/null
+then
+	echo "git, cargo and dotnet are rquired for compilation" 1>&2
+	exit 1
+fi
+
+# Work out the platform we are building on, and the RID which goes
+# along with it.
+case `uname` in
+	Darwin) rid="osx"
+			  platform="Darwin"
+			  ;;
+	Linux) rid="linux"
+			 platform="Linux"
+			 ;;
+	*) echo "Unrecognised platform `uname`" 1>&2
+	   exit 1;
+	   ;;
+esac
+
+# log out the RID decided on
+echo "Compiling for $rid on $platform"
 
 # Make sure the source is up to date
 git submodule init && git submodule update
@@ -18,4 +40,4 @@ mkdir -p contents/runtimes/$rid/native/
 cp rure/regex-capi/target/release/librure* contents/runtimes/$rid/native/
 
 # Pack it all up
-nuget pack IronRure.Batteries-$platform.nuspec
+dotnet pack --no-build IronRure.Batteries-$platform.csproj -o .
