99014101d18094baf4c35819b572472a9a5998a6
Will Speak <lithiumflame@gmail.com>
2017-12-13 19:52:14 +0000

    Change Directory Based on Batch Location
    This works around some versions of the toolset which set the working
    directory.

--- buildi686.bat
+++ buildi686.bat
@@ -1,3 +1,3 @@
 call "%VS150COMNTOOLS%\..\..\VC\Auxiliary\Build\vcvars32.bat"
-pushd rure\regex-capi
+pushd %~dp0\rure\regex-capi
 cargo build --release
--- buildx86_64.bat
+++ buildx86_64.bat
@@ -1,3 +1,3 @@
 call "%VS150COMNTOOLS%\..\..\VC\Auxiliary\Build\vcvars64.bat"
-pushd rure\regex-capi
+pushd %~dp0\rure\regex-capi
 cargo build --release
