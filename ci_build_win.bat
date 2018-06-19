94eb0c1746b5b003c5ae899f032e20e0ccfae07a
hanabi1224 <hailmu@microsoft.com>
2018-06-18 19:56:40 -0700

    windows ci

---
+++ ci_build_win.bat
@@ -0,0 +1,19 @@
+pushd %~dp0\rure\regex-capi
+
+SET VCTOOLPATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\
+echo "%VCTOOLPATH%
+
+call "%VCTOOLPATH%\vcvars64.bat"
+%~dp0\rust-nightly-x86_64-pc-windows-msvc.exe /VERYSILENT /NORESTART /DIR="C:\Program Files (x86)\Rust\"
+cargo clean
+cargo build --release
+xcopy ..\target\release\rure.dll %~dp0\\runtimes\win-x64\native\
+
+
+call "%VCTOOLPATH%\vcvars32.bat"
+%~dp0\rust-nightly-i686-pc-windows-msvc.exe /VERYSILENT /NORESTART /DIR="C:\Program Files (x86)\Rust\"
+cargo clean
+cargo build --release
+xcopy ..\target\release\rure.dll %~dp0\runtimes\win-x86\native\
+
+dotnet build IronRure.Batteries-Windows.csproj
