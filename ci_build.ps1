c6423a1074ed471ad572ff2056458cd33024f85a
hanabi1224 <harlowmoo@gmail.com>
2018-06-19 04:36:57 -0700

    ps1 build script

---
+++ ci_build.ps1
@@ -0,0 +1,8 @@
+if ($PSVersionTable.Platform -eq "Unix") {
+    ci_build_unix.sh
+}
+else{
+    Start-FileDownload "https://static.rust-lang.org/dist/rust-nightly-x86_64-pc-windows-msvc.exe"
+    Start-FileDownload "https://static.rust-lang.org/dist/rust-nightly-i686-pc-windows-msvc.exe"
+    ci_build_win.bat
+}
