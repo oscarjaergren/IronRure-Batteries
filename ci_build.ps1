5dd6dd8a3e9f779e02ffd2fdb46b93b1011ca895
Will Speak <lithiumflame@gmail.com>
2021-06-08 19:22:55 +0100

    Add Azure Pipelines File
    Comile with Azure Pipelines for Win, Linux, and macOS.

--- ci_build.ps1
+++ ci_build.ps1
@@ -2,7 +2,5 @@ if ($PSVersionTable.Platform -eq "Unix") {
     bash ./ci_build_unix.sh
 }
 else{
-    Start-FileDownload "https://static.rust-lang.org/dist/rust-nightly-x86_64-pc-windows-msvc.exe"
-    Start-FileDownload "https://static.rust-lang.org/dist/rust-nightly-i686-pc-windows-msvc.exe"
     .\ci_build_win.bat
 }
