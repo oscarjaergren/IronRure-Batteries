b2f68b5548c2bc6cc020d84c0d9850df758dc560
Oscar Järgren <31920401+oscarjaergren@users.noreply.github.com>
2025-03-09 00:11:46 +0000

    try

--- ci_build_win.bat
+++ ci_build_win.bat
@@ -11,34 +11,10 @@ git submodule update --init
 
 pushd %~dp0\rure\regex-capi
 
-REM --- Dynamically detect VsDevCmd.bat ---
-set "VS_PATHS=C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat;C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\Tools\VsDevCmd.bat;C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat"
-set "VSDIR="
-for %%p in ("%VS_PATHS:;=" "%") do (
-    echo Checking path: %%~p
-    if exist "%%~p" (
-        set "VSDIR=%%~p"
-        goto :found_vsdevcmd
-    )
-)
-echo VsDevCmd.bat not found.
-exit /b 1
 
-:found_vsdevcmd
-echo Found VsDevCmd.bat at "%VSDIR%"
-
-REM --- Call VsDevCmd.bat to set up the VS environment ---
-call "%VSDIR%"
-
-REM --- Derive VCTOOLPATH from the detected VS location ---
-for %%I in ("%VSDIR%") do (
-    set "VS_ROOT=%%~dpI"
-)
-set "VCTOOLPATH=%VS_ROOT%..\VC\Auxiliary\Build"
-echo Using VC build tools at "%VCTOOLPATH%"
 
 REM --- Build for x64 ---
-call "%VCTOOLPATH%\vcvars64.bat"
+call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 rustup default stable-x86_64-pc-windows-msvc
 cargo --version --verbose
 cargo clean
@@ -52,7 +28,7 @@ xcopy ..\target\release\rure.dll %~dp0\runtimes\win-x64\native\ /Y
 REM xcopy ..\target\release\rure.dll %~dp0\runtimes\win-x64\native\
 
 REM --- Build for x86 ---
-call "%VCTOOLPATH%\vcvars32.bat"
+call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
 rustup default stable-i686-pc-windows-msvc
 cargo --version --verbose
 cargo clean
@@ -62,7 +38,9 @@ REM --- Ensure the target directory exists for x86 ---
 if not exist "%~dp0\runtimes\win-x86\native\" (
     mkdir "%~dp0\runtimes\win-x86\native\"
 )
-xcopy ..\target\release\rure.dll %~dp0\runtimes\win-x86\native\ /Y
+
+REM xcopy ..\target\release\rure.dll %~dp0\runtimes\win-x86\native\ /Y
+xcopy /E /I /Y "contents\runtimes" "runtimes"
 REM xcopy ..\target\release\rure.dll %~dp0\runtimes\win-x86\native\
 
 popd
