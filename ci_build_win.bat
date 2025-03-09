d910db09b57544f3a836f046961258326c69a568
Oscar Järgren <31920401+oscarjaergren@users.noreply.github.com>
2025-03-09 00:23:42 +0000

    Just try

--- ci_build_win.bat
+++ ci_build_win.bat
@@ -11,8 +11,6 @@ git submodule update --init
 
 pushd %~dp0\rure\regex-capi
 
-
-
 REM --- Build for x64 ---
 call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 rustup default stable-x86_64-pc-windows-msvc
@@ -24,8 +22,7 @@ REM --- Ensure the target directory exists for x64 ---
 if not exist "%~dp0\runtimes\win-x64\native\" (
     mkdir "%~dp0\runtimes\win-x64\native\"
 )
-xcopy ..\target\release\rure.dll %~dp0\runtimes\win-x64\native\ /Y
-REM xcopy ..\target\release\rure.dll %~dp0\runtimes\win-x64\native\
+xcopy ..\target\release\rure.dll %~dp0\runtimes\win-x64\native\
 
 REM --- Build for x86 ---
 call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
@@ -39,10 +36,7 @@ if not exist "%~dp0\runtimes\win-x86\native\" (
     mkdir "%~dp0\runtimes\win-x86\native\"
 )
 
-REM xcopy ..\target\release\rure.dll %~dp0\runtimes\win-x86\native\ /Y
-xcopy /E /I /Y "contents\runtimes" "runtimes"
-REM xcopy ..\target\release\rure.dll %~dp0\runtimes\win-x86\native\
+xcopy ..\target\release\rure.dll %~dp0\runtimes\win-x86\native\
 
-popd
-REM pushd %~dp0
+pushd %~dp0
 dotnet build -c Release -o PublishOutput IronRure.Batteries-Windows.csproj
